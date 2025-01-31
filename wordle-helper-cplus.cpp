32013535773ff7f31f779a4fa9e5075ccc5c3cc2
gfike19 <gfike19@gmail.com>
2025-01-31 16:25:42 -0600

    worked through for, for each, while, CLI inupt and output, arrays, random numbers (use of library), and printing values

--- wordle-helper-cplus.cpp
+++ wordle-helper-cplus.cpp
@@ -5,12 +5,35 @@
 #include <array>
 #include <iterator>
 #include <string>
+#include <vector>
+#include <cstdlib>
 
 int main()
 {
  /*   std::cout << "Hello World!\n";*/
+	char repeat = 'y';
+
+	while (true) {
+	srand(time(0));
 	int arr_size = rand() % 10 + 1;
-	printf("Array size is: %d ", arr_size);
-	printf("\n");
+	printf("Array size is: %d\n", arr_size);
+	std::vector<int> arr(arr_size, 0);
+	
+	for (int i = 0; i < arr_size; i++) {
+		int rNum = rand() % 100 + 1;
+		arr[i] = rNum;
+	}
+
+	printf("Elements in array are:\n");
+	for (int i : arr) {
+		printf("%d\n", i);
+	}
+	printf("Repeat?(y/n): ");
+	std::cin >> repeat;
+
+	if (repeat != 'y') {
+		break;
+	}
+	}
 
 }
