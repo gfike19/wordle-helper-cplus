44abe31a6288bc112d23bc5e8c5e86dccf330245
gfike19 <gfike19@gmail.com>
2025-01-31 14:57:59 -0600

    initial commit

---
+++ wordle-helper-cplus.cpp
@@ -0,0 +1,16 @@
+// wordle-helper-cplus.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <array>
+#include <iterator>
+#include <string>
+
+int main()
+{
+ /*   std::cout << "Hello World!\n";*/
+	int arr_size = rand() % 10 + 1;
+	printf("Array size is: %d ", arr_size);
+	printf("\n");
+
+}
